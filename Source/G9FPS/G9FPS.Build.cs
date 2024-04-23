7c854537bde8bca888f7be9f85a71c4a03293780
Magnus-54 <magnkrist@gmail.com>
2024-04-23 16:17:36 +0200

    added basic enemy base

--- Source/G9FPS/G9FPS.Build.cs
+++ Source/G9FPS/G9FPS.Build.cs
@@ -8,7 +8,7 @@ public class G9FPS : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
